c3fecdd35a58cd01596958981f9dae7cdefc3056
Lev Popov <levpopov1@gmail.com>
2020-07-11 04:41:44 +0100

    adds alphabet view

---
+++ src/views/Alphabet.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container-fluid" id="aplhabet">
+    <AllLetters/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AllLetters from '@/components/AllLetters.vue'
+
+export default {
+  name: 'Alphabet',
+  components: {
+    AllLetters,
+  }
+}
+</script>
