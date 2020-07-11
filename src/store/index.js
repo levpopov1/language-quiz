4b7b43e5914ccda845da6df75b1ba72b1d7faabf
Lev Popov <levpopov1@gmail.com>
2020-07-11 04:07:05 +0100

    modularises data store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Alphabet from './modules/alphabet'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    Alphabet
   }
 })
