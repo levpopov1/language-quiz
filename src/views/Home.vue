18d649c36ffd6c2c8b8048ba7be25bfce0d8a327
Lev Popov <levpopov1@gmail.com>
2020-07-11 21:23:56 +0100

    imports quiz view into homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="container-fluid" id="home">
-    <h1>Home</h1>
+    <Quiz/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Quiz from '@/components/Quiz.vue'
 
 export default {
   name: 'Home',
   components: {
-
+    Quiz
   }
 }
 </script>
