14af0950e2d08d0655adc626f63710bce0044cc7
Lev Popov <levpopov1@gmail.com>
2020-07-11 21:24:14 +0100

    implements quiz model

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Alphabet from './modules/alphabet'
+import Quiz from './modules/quiz'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    Alphabet
+    Alphabet,
+    Quiz
   }
 })
