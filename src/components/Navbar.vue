aba5a224019e8e0b6ee03293003654f56cc4b263
Lev Popov <levpopov1@gmail.com>
2020-07-11 21:22:40 +0100

    adds some margin

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
         <router-link to="/" class="navbar-brand">Learn Hangul</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
