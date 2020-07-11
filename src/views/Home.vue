a529501e6709f3b89ed3fb9c75b2ca4c0c5c0beb
Lev Popov <levpopov1@gmail.com>
2020-07-11 04:09:29 +0100

    uses alphabet component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid" id="home">
+    <Alphabet/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Alphabet from '@/components/Alphabet.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Alphabet,
   }
 }
 </script>
