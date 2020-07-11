a4cefb62ae85b22b65ef90dba48d7c20cd7310e6
Lev Popov <levpopov1@gmail.com>
2020-07-11 04:42:25 +0100

    implements navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
+    <Navbar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 export default {
   name: "App",
   components: {
+    Navbar
   }
 }
 </script>
