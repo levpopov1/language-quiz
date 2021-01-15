2ae217a2ef42017b1420c86e460e6bf45092d7d9
Lev Popov <levpopov1@gmail.com>
2021-01-15 21:30:09 +0000

    extracts score into new component

---
+++ src/components/Score.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row justify-content-center align-items-center" id="scorePanel">
+    <div class="col-sm-12 mb-3">
+      <div class="card">
+        <div class="card-body d-flex flex-row justify-content-between">
+          <p class="card-text m-0">Correct: <span class="badge badge-success p-2 ml-2">{{numCorrect}}</span></p>
+          <p class="card-text m-0 d-none d-md-block">Question {{ index+1 }} / {{ limit }}</p>
+          <p class="card-text m-0">Wrong: <span class="badge badge-danger p-2 ml-2">{{numWrong}}</span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapState } from "vuex";
+  export default {
+    name: "Score",
+    computed: {
+      ...mapState('Quiz', ["limit", "index", "numCorrect", "numWrong"]),
+    }
+  }
+</script>
+
+<style>
+
+</style>
