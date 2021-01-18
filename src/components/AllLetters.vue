0a799ee6cfd6a90e231a3461fbf5edecc362fb29
Lev Popov <levpopov1@gmail.com>
2021-01-18 19:58:28 +0000

    imaplements letter component. applys row sizing based on letter widths

--- src/components/AllLetters.vue
+++ src/components/AllLetters.vue
@@ -2,17 +2,8 @@
   <div>
     <div v-for="(letterType, index) in letterTypes" v-bind:key="index">
       <h1 class="title">{{ letterType }}</h1>
-      <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 mb-4">
-        <div class="col mb-4" v-for="item in getLetters(letterType)" v-bind:key="item.id">
-          <div  class="card hoverable h-100">
-            <div class="card-body">
-              <h1 class="display-10 text-center">{{ item[lang] }}</h1>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item display-5 text-center">{{ item.en }}</li>
-            </ul>
-          </div>
-        </div>
+      <div class="row row-cols-md-3 mb-4" v-bind:class="rowCols(letterType)">
+        <Letter v-for="item in getLetters(letterType)" v-bind:key="item._id" v-bind:item="item" v-bind:lang="lang"/>
       </div>
     </div>
   </div>
@@ -20,23 +11,34 @@
 
 <script>
 import { mapGetters, mapState, mapActions } from "vuex";
+import Letter from '@/components/Letter';
 export default {
   name: "AllLetters",
-  data(){
-    return{
-      rowCols: 0
-    }
+  components:{
+    Letter
   },
   methods: {
     ...mapActions('Quiz', ["fetchAlphabet"]),
     getLetters(letterType){
       return this.getLettersByType(letterType)
+    },
+    rowCols(letterType){
+      let narrowLetters = ["consonant", "vowel", "monograph", "diacritic", "double-consonant", "double-vowel"];
+      let wideLetters = ["diagraph", "diagraph with diacritic"];
+      let isNarrow = narrowLetters.includes(letterType);
+      let isWide = wideLetters.includes(letterType);
+      return {
+        'row-cols-1': isWide ? true : false,
+        'row-cols-2': isNarrow ? true : false,
+        'row-cols-lg-3': isWide ? true : false,
+        'row-cols-lg-4': this.lang === "kr" ? true : false,
+        'row-cols-lg-5': this.lang === "jp" && isNarrow ? true : false
+      }
     }
   },
   computed: {
     ...mapGetters('Quiz', ["getLettersByType"]),
     ...mapState('Quiz', ["alphabet", "letterTypes", "lang"]),
-
   },
   created(){
     this.fetchAlphabet();
