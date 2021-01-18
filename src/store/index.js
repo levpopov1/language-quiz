09465d4ec697561a925866fb32256a663f9b385c
Lev Popov <levpopov1@gmail.com>
2021-01-18 18:02:28 +0000

    removes unused import

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Alphabet from './modules/alphabet'
 import Quiz from './modules/quiz'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    Alphabet,
     Quiz
   }
 })
