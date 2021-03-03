194fb6ef2a32a67e873f208c6bad887a30555c66
Lev Popov <levpopov1@gmail.com>
2021-03-03 19:27:47 +0000

    changes title on language selection

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
       <div class="container">
-        <router-link to="/" class="navbar-brand">Learn Hangul</router-link>
+        <router-link to="/" class="navbar-brand">Learn <span class="capitalize">{{ language }}</span></router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -54,7 +54,7 @@ export default {
     ...mapActions('Quiz', ["setLimit", "changeLanguage"]),
   },
   computed: {
-    ...mapState('Quiz', ["limit", "languageDisplayString"]),
+    ...mapState('Quiz', ["language", "limit", "languageDisplayString"]),
   },
 }
 </script>
